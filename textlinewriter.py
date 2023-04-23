626a416f2954760660db223a3b161b44e1277ab1
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-23 21:33:02 +0800

    plotting pseudocodes

---
+++ textlinewriter.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+
+# Define a function that writes multiple lines of text to a file named mylife.txt
+
+    # Open the file in append mode to allow writing new lines to it without deleting previous ones
+
+        # Prompt the user to enter a line of text
+ 
+        # Write the user's input to the file
+
+# Define the main function that will be executed when the program is run
+
+    # Display a header for the program
+
+    # Call the line_writer function to write the first line of text to the file
+
+    # Prompt the user to enter more lines of text or quit the program
+
+            # If the user chooses to enter another line, call the line_writer function again
+         
+            # If the user chooses to quit, display a message indicating that the file is ready and exit the program
+            
+            # If the user enters an invalid choice, display an error message and prompt them again
+
+# Call the main function to start the program
