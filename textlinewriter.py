7545191f805ca7a46c26c264e2e2c7406ac2f821
Derick Carlo S. Herrera <dericktzy@gmail.com>
2023-04-23 21:48:54 +0800

    Prompt the user to enter more lines of text or quit the program

--- textlinewriter.py
+++ textlinewriter.py
@@ -19,12 +19,19 @@ def main():
 
      # Call the line_writer function to write the first line of text to the file
     line_writer()
-    # Prompt the user to enter more lines of text or quit the program
-
+    
+     # Prompt the user to enter more lines of text or quit the program
+    while True:
+        user_choice = input("Do you want to add another line? (y/n): ")
+        if user_choice.lower() == "y":
             # If the user chooses to enter another line, call the line_writer function again
-         
+            line_writer()
+        elif user_choice.lower() == "n":
             # If the user chooses to quit, display a message indicating that the file is ready and exit the program
-            
+            print("Your file is ready! Thank you for using Text Line Writer.")
+            break
+        else:
             # If the user enters an invalid choice, display an error message and prompt them again
+            print("Invalid choice. Please enter 'y' or 'n'.")
 
 # Call the main function to start the program
